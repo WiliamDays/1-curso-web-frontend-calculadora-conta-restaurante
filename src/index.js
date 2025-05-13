d82aaa7d0441b6b0852d69c4a4dc8b8a4dc83425
Wiliam Pereira Dias <80425917+WiliamDays@users.noreply.github.com>
2025-05-13 08:24:13 -0300

    Validação de forma de  pagamento refatorado para função

--- src/index.js
+++ src/index.js
@@ -8,6 +8,20 @@ const numeroPessoas = readline.questionInt('Digite o número de pessoas na mesa:
 const valorTotal = readline.questionFloat('Digite o valor total da conta: R$');
 
 // Capturando o método de pagamento
+let mtdPag
+function formaPagamento(){
+    do {
+        mtdPag = readline.questionInt(`Escolha a forma de pagamento (PIX, dinheiro ou cartão): 
+            Digite [1] para pagamento no pix
+            Digite [2] para pagamento no cartão
+            Digite [3] para pagamento no dinheiro            
+        `
+        )} while (mtdPag != 1 && mtdPag != 2 && mtdPag != 3)
+}
+
+formaPagamento()
+
+/*
 let mtdPag;
 
 //Validação para conferir se o que foi digitado pelo usuário é realmente uma das 3 formas de pagamento
@@ -19,10 +33,11 @@ do{
             console.log("valor inválido. Por favor prencha com uma das opções de pagamento."); 
         };
 } while(mtdPag !== "pix" && mtdPag !== "cartao" && mtdPag !== "dinheiro"); //Não consegui realizar a validação com a palavra cartão porque o código estava dando um erro de enconding (não estava reconhecendo o ~) 
+*/
 
 //Faça sua lógica para aplicar o desconto apenas para PIX OU DINHEIRO
 let valorPagPorPessoa;
-if(mtdPag === "pix" || mtdPag === "dinheiro"){
+if(mtdPag === 1 || mtdPag === 3){
     let desconto = (valorTotal/numeroPessoas) * 10/100;
     valorPagPorPessoa = (valorTotal/numeroPessoas) - desconto;
 }
